285da08cb7923dab5dd34c70e6b73ad52d3b5d04
Hussein Bitambuka <76148389+HusseinBitambuka@users.noreply.github.com>
2023-05-31 15:29:42 -0400

    merge sort

--- Sorting_Algorithms/Sorting_Python/BeltOfUtility.py
+++ Sorting_Algorithms/Sorting_Python/BeltOfUtility.py
@@ -9,14 +9,71 @@ def insertion_sort(A)->list:
             i=i-1
         A[i+1]=key # take the element that comes after the current key as the new key
 
+# Buble Sort
+
 def buble_sort(A)->list:
-    for i in range(len(A)-1):
-        
+    '''The buble sort algorithm sorts an array of data by checking if the nth element is smaller than n-1th element and swap their position.
+    the idea here is make sure that the element on the right side of the key are all greater than the key.
+    by reapiting this process for all the n elements in an array, we end up with a sorted array'''
+    for i in range(len(A)-1): 
         for j in range(len(A)-1,i,-1):
             if A[j]<A[j-1]:
-                A[j],A[j-1]=A[j-1],A[j]
-
+                A[j],A[j-1]=A[j-1],A[j] # swap the nth element with the n-1th element
     
+# Merge sort
+
+'''Merge sort algorithm follows the divide and conquer approach. It has the divide, conquer, and combine approach
+'''
+
+
+def merge(A,p,q,r):
+    '''This is the conquer part of the merge sort.
+    '''
+    leftLenght=q-p 
+    rightLenght=r-q
+    left=[]
+    right=[]
+    for i in range(leftLenght): # fill up the element in the left array
+        left.append(A[i])
+    for j in range(rightLenght): # fill up the elemet of the right array
+        right.append(A[j])
+
+    # 
+    i,j=0
+    k=p
+    while(i<leftLenght and j<rightLenght): # if either of the array is empty, terminate the loop
+        if(left[i]<right[i]): # check to see which between the left and the right element goes first in the array
+            A[k]=left[i]
+            i=i+1
+        else:
+            A[k]=right[j]
+            j=j+1
+        k=k+1
+# this step fill up whatever elements are left in the array
+    while(i<leftLenght):
+        A[k]=left[i]
+        i=i+1
+        k=k+1
+    while(j<rightLenght):
+        A[k]=right[i]
+        j=j+1
+        k=k+1
+
+
+def merge_sort(A,p,r):
+    '''This is the devide and combine part of the merge sort algoritm'''
+    if p<r:
+        q=[(p+r)/2]
+        merge_sort(A,p,q)
+        merge_sort(A,q+1,r)
+        merge(A,p,q,r)
+        
+def mergeSort(A):
+    "this is a helper method for the user"
+    p=len(A)
+    r=0
+    merge_sort(A,p,r)
+
 
 #Examples
 StringEXample=["Mtu Mwema","New York","California","Bujumburan","Asiatique","Gishingano","Nyakazu"]
@@ -42,3 +99,15 @@ buble_sort(StringEXample1)
 print("\n After sorting with buble sort \n")
 print("---------------------------------")
 print(StringEXample1)
+
+StringEXample2=["Mtu Mwema","New York","California","Bujumburan","Asiatique","Gishingano","Nyakazu"]
+
+print("Before sorting \n")
+print("---------------------------------")
+print(StringEXample2)
+
+mergeSort(StringEXample2)
+
+print("\n After sorting with merge sort \n")
+print("---------------------------------")
+print(StringEXample2)
