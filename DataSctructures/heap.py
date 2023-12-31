e691e61ac40195a97c59d27f40f13ec7d62cf109
Hussein Bitambuka <76148389+HusseinBitambuka@users.noreply.github.com>
2023-12-31 01:32:20 -0500

    Min heap and K closet points to the origin problem

--- DataSctructures/heap.py
+++ DataSctructures/heap.py
@@ -1,3 +1,4 @@
+import math
 class MaxHeap():
     def __init__(self):
         self.capacity = 100
@@ -63,4 +64,75 @@ heap.insert(200)
 print(heap.extract_max())
 
 heap.print_heap()
+
+
+# fun with Min Heap leetcode 973
+
+class Minheap():
+    def __init__(self, capacity):
+        self.heap = [None]*capacity
+        self.size = 0
+
+    def heapifyUp(self, index):
+        '''maintains the heap property by swapping the parent with the child if the parent is greater than the child'''
+        if index == 0:
+            return
+        parent = (index - 1) // 2
+        if self.heap[parent][0] > self.heap[index][0]:
+            self.heap[parent], self.heap[index] = self.heap[index], self.heap[parent]
+            self.heapifyUp(parent)
+    def heapifyDown(self, index):
+        '''maintains the heap property by swapping the parent with the child if the parent is greater than the child'''
+        left = (index * 2) + 1 # left child
+        right = (index * 2 ) + 2 # right child
+        smallest = index # parent
+        
+        if self.size > left and self.heap[smallest][0] > self.heap[left][0]: # if the left child is smaller than the parent, set the smallest to the left child
+            smallest = left
+        if self.size > right and self.heap[smallest][0] > self.heap[right][0]: # if the right child is smaller than the parent, set the smallest to the right child
+            smallest = right
+        if smallest != index: # if the parent is not the smallest, swap the parent with the smallest child
+            self.heap[smallest], self.heap[index] = self.heap[index], self.heap[smallest]
+            self.heapifyDown(smallest)
+    def getDistance(self, p):
+        return math.sqrt(p[0]**2 + p[1]**2) # returns the distance of the point from the origin
+
+    def insert(self, p):
+        '''inserts a point into the heap'''
+        d = self.getDistance(p) # get the distance of the point from the origin
+        element =  [d, p] # create a list with the distance and the point
+        self.heap[self.size] = element
+        self.heapifyUp(self.size) # maintain the heap property
+        self.size += 1
+
+    def getMin(self):
+        '''returns the minimum element in the heap'''
+        minEl = self.heap[0]
+        self.heap[0] = self.heap[self.size - 1]
+        self.heapifyDown(0)
+        self.size -= 1
+        return minEl
+
+    def returnKC(self, k):
+        '''returns the k closest points to the origin'''
+        arr = [None]*k
+        i = 0
+        while k > 0:
+            arr[i] = self.getMin()[1] # get the point from the minimum element
+            i += 1
+            k -= 1
+        return arr
+
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        minH = Minheap(len(points))
+        for p in points:
+            minH.insert(p)
+        return minH.returnKC(k)
+
 
